3bd74bf842670896e8ee1e4012c71c0ebd8e966c
josoriora <julian.osorior@gmail.com>
2024-12-23 10:33:13 -0500

    - Initial project commit.

---
+++ ObjCAndCPlusPlus/ObjCAndCPlusPlus/ObjCAndCPlusPlus.h
@@ -0,0 +1,18 @@
+//
+//  ObjCAndCPlusPlus.h
+//  ObjCAndCPlusPlus
+//
+//  Created by JULIAN OSORIO RAMIREZ on 23/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ObjCAndCPlusPlus.
+FOUNDATION_EXPORT double ObjCAndCPlusPlusVersionNumber;
+
+//! Project version string for ObjCAndCPlusPlus.
+FOUNDATION_EXPORT const unsigned char ObjCAndCPlusPlusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjCAndCPlusPlus/PublicHeader.h>
+
+
